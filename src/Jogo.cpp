47454fd2e152278e29e169bcf60e016f0f7de370
Wald <kerwald.s@gmail.com>
2026-04-17 21:47:46 -0300

    Implement game round execution and player actions in Jogo class; add methods for round management and input handling.

--- src/Jogo.cpp
+++ src/Jogo.cpp
@@ -23,3 +23,63 @@ Jogo::Jogo( std::vector<Jogador> &jogadores ) : mesa( 0 ), jogadores( jogadores 
     this->mesa.adicionarSaldo( valorInicial );
     iniciar();
 };
+
+void Jogo::iniciar(){
+
+    int rodada{0};
+
+    do{
+        rodada++;
+        executarRodada( rodada );
+
+    }while( !jogoEncerrado );
+    
+    
+};
+
+void Jogo::imprimirStatusMesa() const {
+    std::cout << mesa.getSaldo() << std::endl;
+};
+
+void Jogo::executarRodada( const int numeroRodada ){
+
+    int pote{0};
+
+    for( Jogador j : jogadores ){
+        std::cout << j.getNome() << " "  << j.getSaldo() << std::endl;
+    }
+    imprimirStatusMesa();
+
+    for( Jogador j : jogadores ){
+        j.realizarAposta( solicitarAposta( j, 1 ) );
+        pote += j.getApostaRodadaAtual();
+    }
+
+    for( Jogador j : jogadores ){
+        if( j.isAtivoNaRodada( ) ){
+
+            char jogada;
+            std::cout << numeroRodada << "° Rodada" << std::endl;
+            std::cout << j.getNome() << " faca sua jogada: ";
+            std::cin >> jogada;
+
+            switch ( jogada )
+            {
+            case 'r': // rock = pedra
+                j.setJogadaAtual( Jogada::PEDRA );
+                break;
+            case 'p': // paper = papel
+                j.setJogadaAtual( Jogada::PAPEL );
+            case 's': // scissors = tesoura
+                j.setJogadaAtual( Jogada::TESOURA);
+            default:
+                std::cerr << "VALOR DIGITADO INVALIDO!!!";
+                break;
+            }
+        }
+    }
+
+};
+std::vector<Jogador> Jogo::determinarVencedores( std::vector<Jogador> &ativos ){
+
+};
