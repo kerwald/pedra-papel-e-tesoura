18b77219e20d7ae41be80927a8327578ad9fe149
Wald <kerwald.s@gmail.com>
2026-04-18 16:50:11 -0300

    Refactor Mesa class to improve saldo management; enhance adicionarSaldo and reduzirSaldo methods for better validation and error handling.

--- src/Mesa.cpp
+++ src/Mesa.cpp
@@ -1,4 +1,30 @@
 #include "Mesa.hpp"
+#include <iostream>
 
 Mesa::Mesa( const int saldoInicial ) : saldo( saldoInicial )
 {}
+
+int Mesa::getSaldo() const{
+    return saldo;
+}
+
+void Mesa::adicionarSaldo( const int valor ){
+    if( valor >=0 ){
+        saldo += valor;
+    } else{
+        std::cerr << "valor invalido!!!" << std::endl;
+    }
+}
+
+void Mesa::reduzirSaldo( const int valor ){
+    if( valor >=0 ){
+        if( valor>=saldo ){
+            saldo -= valor;
+        } else{
+            std::cerr << "Saldo insuficiente!!!" << std::endl;
+        }
+        
+    } else{
+        std::cerr << "Valor invalido!!!" << std::endl;
+    }
+}
